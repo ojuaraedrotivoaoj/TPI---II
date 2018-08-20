2e4782d34f4f3e760030aa071176c0c08a70c29b
Micro10 <joao.araujo111@etec.sp.gov.br>
2018-08-20 20:14:27 -0300

    Mensagem - exemplo: Aula 2

---
+++ init.sh
@@ -0,0 +1,5 @@
+git clone git remote add origin https://github.com/ojuaraedrotivoaoj/tpi-ii.git
+git config --global user.email "joao.araujo111@etec.sp.gov.com"
+git config --global user.name "João Vitor de Araujo"
+cd tpi-ii
+code .
